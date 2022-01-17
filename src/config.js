f503e794c86eb98ae89728dd33398b7eab15fdf4
Ben Richeson <benricheson101@gmail.com>
2022-01-17 14:58:15 -0500

    fix: new support link

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 export const constants = {
   PERMISSIONS: 8192, // MANAGE_MESSAGES
-  SUPPORT: 'xfe9tcW',
+  SUPPORT: 'bFDZqcTFt5',
   REGEX: {
     MSG_URL: new RegExp('https?:\/\/(?:canary\.|ptb\.)?discord(?:app)?\.com\/channels\/(?<guild>\\d{16,18})\/(?<channel>\\d{16,18})\/(?<message>\\d{16,18})'),
     SNOWFLAKE: new RegExp('\d{16,18}'),
